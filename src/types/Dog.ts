c6fae674928fa353db5fda1fc3e65c0ec56a64b9
Rían Errity <rian@paradaux.io>
2025-07-29 19:24:22 +0100

    Fix install script

--- src/types/Dog.ts
+++ src/types/Dog.ts
@@ -8,7 +8,7 @@ export interface Dog {
     dog_status: string | null;
     dog_current_handler: string | null;
     dog_general_notes: string | null;
-    dog_is_active: boolean;
+    dog_is_archived: boolean;
     dog_created_at: string;
     dog_updated_at: string;
     dog_created_by: string;
