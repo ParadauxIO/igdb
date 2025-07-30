146600e6493bd3e874d89d18c685420e019bf4d4
Rían Errity <rian@paradaux.io>
2025-07-30 12:57:55 +0100

    Tidy up AdminDogView significantly

---
+++ src/partials/dog.ts
@@ -0,0 +1,60 @@
+import {supabase} from "../state/supabaseClient.ts";
+import type {Dog} from "../types/Dog.ts";
+
+export const getDogsWithNames = async () => {
+    const {data, error} = await supabase
+        .from('dogs')
+        .select(`
+            *,
+            dog_created_by_user:users!dogs_dog_created_by_fkey (
+                name
+            ),
+            dog_last_edited_by_user:users!dogs_dog_last_edited_by_fkey (
+                name
+            ),
+            dog_current_handler_user:users!dogs_dog_current_handler_fkey (
+                name
+            )
+        `)
+        .order('dog_created_at', {ascending: false});
+
+    if (error) {
+        console.log("Error occurred while fetching dogs:", error);
+        return;
+    }
+
+    if (data) {
+        const flattenedData: Dog[] = data.map((dog: any) => ({
+            ...dog,
+            dog_created_by_name: dog.dog_created_by_user?.name,
+            dog_last_edited_by_name: dog.dog_last_edited_by_user?.name,
+            dog_current_handler_name: dog.dog_current_handler_user?.name,
+        }));
+        console.log(flattenedData);
+        return flattenedData;
+    }
+};
+
+export const deleteDog = async (dogId: string) => {
+    const {error} = await supabase
+        .from('dogs')
+        .delete()
+        .eq('dog_id', dogId);
+
+    if (error) {
+        console.error("Failed to delete dog:", error);
+        return;
+    }
+};
+
+export const archiveDog = async (dogId: string) => {
+    const {error} = await supabase
+        .from('dogs')
+        .update({dog_is_active: false})
+        .eq('dog_id', dogId);
+
+    if (error) {
+        console.error("Failed to disable dog:", error);
+        return;
+    }
+}
--- src/types/Dog.ts
+++ src/types/Dog.ts
@@ -7,12 +7,20 @@ export interface Dog {
     dog_picture: string | null;
     dog_status: string | null;
     dog_current_handler: string | null;
+    dog_current_handler_user?: DogHandlerName;
+    dog_current_handler_name?: string;
     dog_general_notes: string | null;
     dog_is_archived: boolean;
     dog_created_at: string;
     dog_updated_at: string;
     dog_created_by: string;
+    dog_created_by_user?: DogHandlerName;
     dog_created_by_name?: string;
     dog_last_edited_by: string;
+    dog_last_edited_by_user?: DogHandlerName; // <-- missing
     dog_last_edited_by_name?: string;
+}
+
+export type DogHandlerName = {
+    name: string;
 }
